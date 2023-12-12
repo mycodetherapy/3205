e71f78fcce985046775e1646636ba81179d4a4ac
Max Ovchinnikov <ovchinnicov132@gmail.com>
2023-12-12 23:29:19 +0300

    add validation for client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 interface UserData {
   email: string;
@@ -8,35 +8,72 @@ interface UserData {
 
 const App: React.FC = () => {
   const [email, setEmail] = useState<string>("");
-  const [number, setNumber] = useState<string>("");
+  let [number, setNumber] = useState<string>("");
   const [result, setResult] = useState<UserData[]>([]);
   const [loading, setLoading] = useState<boolean>(false);
+  const [validNumber, setValidNumber] = useState<boolean>(true);
+  const [validEmail, setValidEmail] = useState<boolean>(true);
+  const [emptyEmail, setEmptyEmail] = useState<boolean>(true);
+  const [isSubmited, setIsSubmited] = useState<boolean>(false);
   const abortControllerRef = useRef<AbortController | null>(null);
 
   const formatNumber = (value: string): string => {
-    const cleanedValue = value.replace(/\D/g, "");
-    const formattedValue = cleanedValue.replace(
-      /(\d{2})(\d{2})(\d{2})/g,
-      "$1-$2-$3"
-    );
+    const cleanedValue = value.replace(/[^\d-]/g, "");
+    let formattedValue = cleanedValue.replace(/(\d{2})(\d{2})/g, "$1-$2");
+
+    if (formattedValue.length >= 8) {
+      formattedValue = formattedValue.substring(0, 8);
+    }
+
     return formattedValue;
   };
 
   const validateNumber = (value: string): boolean => {
-    const cleanedValue = value.replace(/\D/g, "");
+    const cleanedValue = value.replace(/[^\d-]/g, "");
     return /^(\d{2}-\d{2}-\d{2}|\d{6})$/.test(cleanedValue);
+  };
+
+  const validateEmail = (value: string) => {
+    return /^((.*\s)?<[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+\.[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+>|[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+\.[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+)$/.test(
+      value
+    );
   };
 
   const handleNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const formattedValue = formatNumber(e.target.value);
+    if (validateNumber(formattedValue)) {
+      setValidNumber(true);
+    } else {
+      setValidNumber(false);
+    }
     setNumber(formattedValue);
   };
 
+  const handleEmailChane = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const emailValue = e.target.value.trim();
+    if (validateEmail(emailValue)) {
+      setValidEmail(true);
+    } else {
+      if (emailValue === "") {
+        setEmptyEmail(true);
+        setValidEmail(true);
+      } else {
+        setEmptyEmail(false);
+        setValidEmail(false);
+      }
+    }
+    setEmail(emailValue);
+  };
+
+  useEffect(() => {
+    validNumber ? console.log("Validno") : console.log("Not");
+  }, [validNumber]);
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
-    if (!email) {
-      alert("Пожалуйста, введите email.");
+    setIsSubmited(true);
+    if (!validNumber || !validEmail || emptyEmail) {
+      alert("The form failed validation!");
       return;
     }
 
@@ -50,6 +87,7 @@ const App: React.FC = () => {
     setLoading(true);
 
     try {
+      number = number.replace(/\D/g, "");
       const response = await fetch("http://localhost:3001/api/search", {
         method: "POST",
         headers: {
@@ -61,11 +99,12 @@ const App: React.FC = () => {
 
       const data: UserData[] = await response.json();
       setResult(data);
+      setIsSubmited(false);
     } catch (fetchError: any) {
       if (fetchError.name === "AbortError") {
-        console.log("Запрос отменен.");
+        console.log("Request canceled.");
       } else {
-        console.error("Ошибка при запросе:", fetchError);
+        console.error("Query error:", fetchError);
       }
     } finally {
       setLoading(false);
@@ -76,26 +115,14 @@ const App: React.FC = () => {
     <div>
       <form onSubmit={handleSubmit}>
         <label>Email:</label>
-        <input
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
+        <input type="email" value={email} onChange={handleEmailChane} />
+        {emptyEmail && isSubmited && <p>Required field</p>}
+        {!validEmail && isSubmited && <p>Invalid email</p>}
         <br />
 
         <label>Number:</label>
-        <input
-          type="text"
-          value={number}
-          onChange={(e) =>
-            setNumber(
-              e.target.value
-                .replace(/\D/g, "")
-                .replace(/(\d{2})(\d{2})/, "$1-$2")
-            )
-          }
-        />
+        <input type="text" value={number} onChange={handleNumberChange} />
+        {!validNumber && isSubmited && <p>Invalid number</p>}
         <br />
 
         <button type="submit">
@@ -103,14 +130,17 @@ const App: React.FC = () => {
         </button>
       </form>
 
-      {loading && <p>Загрузка...</p>}
+      {loading && <p>Loading...</p>}
       {result.length > 0 && (
         <div>
           <p>Query results:</p>
           {result.map((userData, index) => (
-            <p key={index}>{`${index + 1}. "email": "${
+            <p key={index}>{`${index + 1}. email: ${
               userData.email
-            }", "number": "${userData.number}"`}</p>
+            }, phone number: ${userData.number.replace(
+              /(\d{2})(\d{2})(\d{2})/g,
+              "$1-$2-$3"
+            )}`}</p>
           ))}
         </div>
       )}
